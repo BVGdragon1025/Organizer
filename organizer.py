b966d2910ee01a06815264ffd1568472388a66f4
BVGdragon1025 <72506695+BVGdragon1025@users.noreply.github.com>
2021-01-02 22:09:07 +0100

    - Dodano funckję sortowania zadań od tych najbliższych - Przeorganizowano praktycznie wszystko - Dzięki temu uzyskałem zabawny efekt uboczny - Każdy user od teraz ma swój własny plik z notatkami i zadaniami (jeśli nie istnieją, zostaną automatycznie utworzone) - Dodałem kilka komentarzy, więcej wkrótce

--- organizer.py
+++ organizer.py
@@ -2,48 +2,64 @@ import datetime  # Użyję później do sortowania, lub czegoś innego
 
 import os.path  # Przydatne do paru rzeczy :)
 
+
 #  import tkinter : Na ten moment tego nie używam
 #  Plus moja wiedza o klasach i obiektach jest na poziomie przypominania sobie
 
 
-class Organizer:
-    """def dane_user(self,user,psw):
+class Organizer:  # Użyłem klasy tylko dlatego, że mogę odnosić się do dowolnej funkcji kiedy tylko chcę
+
+    def __init__(self):  # Użyłem tego tylko dlatego, by móc odnosić się do nazwy użytkownika.
+        # To było najprostrze rozwiązanie dla mnie
         user = str(input("Podaj nazwę użytkownika: "))
         psw = str(input("Podaj hasło: "))
-"""
+        self.user = user
+        self.psw = psw
+
+    def main(self):
+        while True:
+            choice = str(input("Wybierz co chcesz zrobić: [1]Rejestracja, [2]Logowanie, [q]Wyjście: "))
+            if choice == "1":
+                Organizer.rejestracja(self)
+            elif choice == "2":
+                Organizer.logowanie(self)
+            elif choice == "q" or choice.lower == "q" or choice.upper() == "Q":
+                print("Żegnaj!")
+                break
+            else:
+                print("Zły wybór!")
+
     def rejestracja(self):
-        user = str(input("Podaj nazwę użytkownika: "))
-        psw = str(input("Podaj hasło: "))
-        with open("users.txt", "r") as users:
-            readusers = users.read()
-            if os.path.exists("users.txt"):
-                if user.lower() in readusers:
+        if os.path.exists("users.txt"):
+            with open("users.txt", "r") as users:
+                readusers = users.read()
+                if self.user.lower() in readusers:
                     print("Podany użytkownik istnieje. Uzyj opcji \"Logowanie\"")
-                elif user in readusers:
+                elif self.user in readusers:
                     print("Podany użytkownik istnieje. Uzyj opcji \"Logowanie\"")
                 else:
                     with open("users.txt", "a") as addusers:
-                        addusers.write(user.lower() + " " + psw + "\n")
+                        addusers.write(self.user.lower() + " " + self.psw + "\n")
                         print("Użytkownik dodany!")
-            else:
-                with open("users.txt", "x") as createusers:
-                    createusers.write(user.lower() + " " + psw + "\n")
-                    print("Użytkownik dodany!")
+        else:
+            with open("users.txt", "x") as createusers:
+                createusers.write(self.user.lower() + " " + self.psw + "\n")
+                print("Użytkownik dodany!")
 
     def logowanie(self):
-        user = str(input("Podaj nazwę użytkownika: "))
-        psw = str(input("Podaj hasło: "))
-        with open("users.txt", "r") as users:
-            checkusers = users.readlines()
-            if (user.lower() + " " + psw + "\n") in checkusers:
-                print("Witaj, " + user + "!")
-                Organizer.wybor_opcji(self)
-            elif (user + " " + psw + "\n") in checkusers:
-                print("Witaj, " + user + "!")
-                Organizer.wybor_opcji(self)
-            else:
-                print("Zły login lub hasło!")
-        return user
+        if os.path.exists("users.txt"):
+            with open("users.txt", "r") as users:
+                checkusers = users.readlines()
+                if (self.user.lower() + " " + self.psw + "\n") in checkusers:
+                    print("Witaj, " + self.user + "!")
+                    Organizer.wybor_opcji(self)
+                elif (self.user + " " + self.psw + "\n") in checkusers:
+                    print("Witaj, " + self.user + "!")
+                    Organizer.wybor_opcji(self)
+                else:
+                    print("Zły login lub hasło!")
+        else:
+            print("Brak użytkowników! Użyj opcji \"Zarejestruj\"")
 
     def wybor_opcji(self):
         while True:
@@ -65,6 +81,7 @@ class Organizer:
                               "[2]Wyświetl zadania, "
                               "[3]Usuń zadanie, "
                               "[4]Usuń wszystkie zadania, "
+                              "[5]Wyświetl zadania (od nadchodzących) "
                               "[q]Wyjdź z menu: "))
             if menu2 == "1":
                 Organizer.dodaj_zadanie(self)
@@ -74,6 +91,8 @@ class Organizer:
                 Organizer.usun_zadanie(self)
             elif menu2 == "4":
                 Organizer.usun_zadania(self)
+            elif menu2 == "5":
+                Organizer.sortowanie_zadan(self)
             elif menu2 == "q" or menu2.lower == "q" or menu2.upper() == "Q":
                 break
             else:
@@ -101,50 +120,60 @@ class Organizer:
 
     def dodaj_notatke(self):
         notatka = str(input("Wpisz swoją notatkę: "))
-        if os.path.exists("notes/notes.txt"):
-            with open("notes/notes.txt", "a") as addnote:
-                addnote.write(notatka + "\n")
-                print("Notatka dodana!")
+        if os.path.exists("notes"):
+            if os.path.exists("notes/%s.txt" % self.user.lower()):
+                with open("notes/%s.txt" % self.user.lower(), "a") as addnote:
+                    addnote.write(notatka + "\n")
+                    print("Notatka dodana!")
+            else:
+                with open("notes/%s.txt" % self.user.lower(), "x") as addnote:
+                    addnote.write(notatka + "\n")
+                    print("Notatka dodana!")
         else:
             os.makedirs("notes")
-            with open("notes/notes.txt", "x") as addnote:
+            with open("notes/%s.txt" % self.user.lower(), "x") as addnote:
                 addnote.write(notatka + "\n")
                 print("Notatka dodana!")
 
     def dodaj_zadanie(self):
         zadanie = str(input("Wpisz zadanie: "))
         data = str(input("Wpisz datę (RRRR-MM-DD): "))
-        if os.path.exists("tasks/tasks.txt"):
-            with open("tasks/tasks.txt", "a") as addtask:
-                addtask.write(zadanie + " " + data + "\n")
-                print("Zadanie dodane!")
+        if os.path.exists("tasks"):
+            if os.path.exists("tasks/%s.txt" % self.user.lower()):
+                with open("tasks/%s.txt" % self.user.lower(), "a") as addtask:
+                    addtask.write(data + " " + zadanie + "\n")
+                    print("Zadanie dodane!")
+            else:
+                with open("tasks/%s.txt" % self.user.lower(), "x") as addtask:
+                    addtask.write(data + " " + zadanie + "\n")
+                    print("Zadanie dodane!")
         else:
             os.makedirs("tasks")
-            with open("tasks/tasks.txt", "x") as addtask:
-                addtask.write(zadanie + " " + data + "\n")
+            with open("tasks/%s.txt" % self.user.lower(), "x") as addtask:
+                addtask.write(data + " " + zadanie + "\n")
                 print("Zadanie dodane!")
 
     def wyswietl_notatki(self):
-        if os.path.exists("notes/notes.txt"):
-            with open("notes/notes.txt", "r") as readnotes:
+        if os.path.exists("notes/%s.txt" % self.user.lower()):
+            with open("notes/%s.txt" % self.user.lower(), "r") as readnotes:
                 for lines in readnotes:
-                    print(lines)
+                    print(lines.rstrip())
         else:
             print("Brak notatek!")
 
     def wyswietl_zadania(self):
-        if os.path.exists("tasks/tasks.txt"):
-            with open("tasks/tasks.txt", "r") as readtasks:
+        if os.path.exists("tasks/%s.txt" % self.user.lower()):
+            with open("tasks/%s" % self.user.lower(), "r") as readtasks:
                 for lines in readtasks:
-                    print(lines)
+                    print(lines.rstrip())
         else:
             print("Brak notatek!")
 
     def usun_zadania(self):
-        if os.path.exists("tasks/tasks.txt"):
+        if os.path.exists("tasks/%s.txt" % self.user):
             check = str(input("Czy chcesz trwale usunąć wszystkie zadania? [y]Tak, [n]Nie: "))
             if check == "y" or check.lower() == "y" or check.upper() == "Y":
-                with open("tasks/tasks.txt", "w") as deletetasks:
+                with open("tasks/%s.txt" % self.user, "w") as deletetasks:
                     deletetasks.close()
                 print("Zadania zosatały usunięte!")
             elif check == "n" or check.lower() == "n" or check.upper() == "N":
@@ -153,24 +182,24 @@ class Organizer:
             print("Brak zadań do usunięcia!")
 
     def usun_zadanie(self):
-        if os.path.exists("tasks/tasks.txt"):
+        if os.path.exists("tasks/%s.txt" % self.user):
             dtask = str(input("Wpisz nazwę zadania: "))
             dtaskdate = str(input("Wpisz datę tego zadania (RRRR-MM-DD): "))
-            with open("tasks/tasks.txt", "r") as readtask:
+            with open("tasks/%s.txt" % self.user, "r") as readtask:
                 readtaskls = readtask.readlines()
-            with open("tasks/tasks.txt", "w") as writelines:
+            with open("tasks/%s.txt" % self.user, "w") as writelines:
                 for line in readtaskls:
-                    if line.strip("\n") != (dtask + " " + dtaskdate):
+                    if line.strip("\n") != (dtaskdate + " " + dtask):
                         writelines.write(line)
                         print("Zadanie usunięte!")
         else:
             print("Brak zadań do usunięcia!")
 
     def usun_notatki(self):
-        if os.path.exists("notes/notes.txt"):
+        if os.path.exists("notes/%s.txt" % self.user):
             check2 = str(input("Czy chcesz trwale usunąć wszystkie notatki? [y]Tak, [n]Nie: "))
             if check2 == "y" or check2.lower() == "y" or check2.upper() == "Y":
-                with open("notes/notes.txt", "w") as deletenotes:
+                with open("notes/%s.txt" % self.user, "w") as deletenotes:
                     deletenotes.close()
                 print("Notatki zostały usunięte!")
             elif check2 == "n" or check2.lower() == "n" or check2.upper() == "N":
@@ -179,26 +208,27 @@ class Organizer:
             print("Brak notatek do usunięcia!")
 
     def usun_notatke(self):
-        if os.path.exists("notes/notes.txt"):
+        if os.path.exists("notes/%s.txt" % self.user):
             dnote = str(input("Wpisz którą notatkę usunąć: "))
-            with open("notes/notes.txt", "r") as readnote:
+            with open("notes/%s.txt" % self.user, "r") as readnote:
                 readnotels = readnote.readlines()
-            with open("notes/notes.txt", "w") as writelines:
+            with open("notes/%s.txt" % self.user, "w") as writelines:
                 for line in readnotels:
                     if line.strip("\n") != dnote:
                         writelines.write(line)
                         print("Notatka usunięta!")
 
+    def sortowanie_zadan(self):
+        with open("tasks/%s.txt" % self.user, "r") as readtasks:
+            task_temp = []
+            for line in readtasks:
+                temp_lines = line.splitlines()
+                for i in temp_lines:
+                    task_temp.append(i)
+            task_temp.sort()
+            for i in task_temp:
+                print(i)
+            task_temp.clear()
 
-while True:
-    choice = str(input("Wybierz co chcesz zrobić: [1]Rejestracja, [2]Logowanie, [q]Wyjście: "))
-    lg = Organizer()
-    if choice == "1":
-        lg.rejestracja()
-    elif choice == "2":
-        lg.logowanie()
-    elif choice == "q" or choice.lower == "q" or choice.upper() == "Q":
-        print("Żegnaj!")
-        break
-    else:
-        print("Zły wybór!")
+
+Organizer().main()
