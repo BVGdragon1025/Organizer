ec19b83157d2a08156243bdb9c8f3de6a899add4
BVGdragon1025 <72506695+BVGdragon1025@users.noreply.github.com>
2021-04-22 20:39:18 +0200

    Zalążki czegoś nowego...

---
+++ organizer2.py
@@ -0,0 +1,14 @@
+﻿import sqlite3
+import os
+conn = sqlite3.connect("organizer.db")
+
+if not os.path.exists("organizer.db"):
+    conn.execute("CREATE TABLE 'notes'("
+                 "'id' INTEGER NOT NULL,"
+                 "'title' TEXT NOT NULL,"
+                 "PRIMARY KEY('id' AUTOINCREMENT);")
+    conn.execute("CREATE TABLE 'tasks'("
+                 "'id' INTEGER NOT NULL,"
+                 "'title' TEXT NOT NULL,"
+                 "'exp_date' TEXT NOT NULL,"
+                 "PRIMARY KEY('id' AUTOINCREMENT);")
